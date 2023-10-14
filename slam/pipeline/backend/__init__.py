7aadaba05f249819eabe163201f35b8aa43ddda1
Pavel Mokeev <pav3l.mokeev@gmail.com>
2023-10-14 10:20:56 +0300

    fix(lint): fix black linter

--- slam/pipeline/backend/__init__.py
+++ slam/pipeline/backend/__init__.py
@@ -3,6 +3,4 @@ import slam.pipeline.backend.eigen_factor as eigen_factor_module
 from slam.pipeline.backend.backend_base import *
 from slam.pipeline.backend.eigen_factor import *
 
-__all__ = (
-    backend_base_module.__all__ + eigen_factor_module.__all__
-)
+__all__ = backend_base_module.__all__ + eigen_factor_module.__all__
