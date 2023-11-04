49fe4241e96ff8c1c375f3c3288b6e5995905b7f
Pavel Mokeev <75482510+pmokeev@users.noreply.github.com>
2023-11-04 16:55:07 +0300

    feat(pipeline): add parameters for run function of pipeline (#17)
    * WIP
    
    * feat(pipeline): add parameters for run function of pipeline

--- slam/pipeline/static.py
+++ slam/pipeline/static.py
@@ -1,9 +1,26 @@
+from dataclasses import dataclass
+from typing import Optional
+
 from octreelib.grid import GridBase
 
 from slam.backend import BackendOutput
-from slam.pipeline.pipeline import Pipeline
+from slam.pipeline.pipeline import Pipeline, PipelineRuntimeParameters
 
-__all__ = ["StaticPipeline"]
+__all__ = ["StaticPipelineRuntimeParameters", "StaticPipeline"]
+
+
+@dataclass
+class StaticPipelineRuntimeParameters(PipelineRuntimeParameters):
+    """
+    Represents list of parameters which will be used in `run` function of pipeline
+
+    Parameters
+    ----------
+    key_point_cloud_number: Optional[int]
+        Represents number of the point cloud for which will be called `subdivide` function
+    """
+
+    key_point_cloud_number: Optional[int] = None
 
 
 class StaticPipeline(Pipeline):
@@ -11,7 +28,9 @@ class StaticPipeline(Pipeline):
     Represents simple static slam implementation
     """
 
-    def run(self, grid: GridBase) -> BackendOutput:
+    def run(
+        self, parameters: StaticPipelineRuntimeParameters, grid: GridBase
+    ) -> BackendOutput:
         """
         Represents base slam algorithm:
         1. Transforms point clouds to global coordinates
@@ -20,6 +39,13 @@ class StaticPipeline(Pipeline):
         4. Runs filter functions to delete unnecessary voxels/point clouds
         5. Runs chosen backend and produce PipelineOutput
 
+        Parameters
+        ----------
+        parameters: StaticPipelineRuntimeParameters
+            Represents parameters for `run` function of pipeline
+        grid: GridBase
+            Grid which will be used for optimizations
+
         Returns
         -------
         output: PipelineOutput
@@ -27,10 +53,22 @@ class StaticPipeline(Pipeline):
         """
         transformed_point_clouds = self._transform_point_clouds()
 
-        for pose_number, point_cloud in enumerate(transformed_point_clouds):
-            grid.insert_points(pose_number, point_cloud.points)
+        if parameters.key_point_cloud_number is None:
+            for pose_number, point_cloud in enumerate(transformed_point_clouds):
+                grid.insert_points(pose_number, point_cloud.points)
 
-        grid.subdivide(self._subdividers)
+            grid.subdivide(self._subdividers)
+        else:
+            grid.insert_points(
+                parameters.key_point_cloud_number,
+                transformed_point_clouds[parameters.key_point_cloud_number].points,
+            )
+            grid.subdivide(self._subdividers)
+
+            for pose_number, point_cloud in enumerate(transformed_point_clouds):
+                if pose_number == parameters.key_point_cloud_number:
+                    continue
+                grid.insert_points(pose_number, point_cloud.points)
 
         for segmenter in self._segmenters:
             grid.map_leaf_points(segmenter)
